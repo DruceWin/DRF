4af5bef7ca9d4b612c15202e1896a1a3bf090dd8
Andrei Kulakov <kulakov1@tut.by>
2022-10-16 10:33:27 +0300

    update react app

--- front_react_app/main/src/App.js
+++ front_react_app/main/src/App.js
@@ -1,15 +1,61 @@
-import React, { Component } from 'react';
+import React, {Component, useEffect, useState} from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello Snezhka</h1>
-      </div>
+const List = (props) => {
+  const { prod } = props;
+  if (!prod || prod.length === 0) return <p>No repos, sorry</p>;
+  return (
+    <ul>
+      <h2 className='list-head'>Available Products</h2>
+      {prod.map((pr) => {
+        return (
+          <li key={pr.id} className='list'>
+            <span className='repo-text'>{pr.title} </span>
+            <span className='repo-description'>{pr.description}</span>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
+function withListLoading(Component) {
+  return function WihLoadingComponent({ isLoading, ...props }) {
+    if (!isLoading) return <Component {...props} />;
+    return (
+      <p style={{ textAlign: 'center', fontSize: '30px' }}>
+        Hold on, fetching data may take some time :)
+      </p>
     );
-  }
+  };
+}
+
+function App() {
+  const ListLoading = withListLoading(List);
+  const [appState, setAppState] = useState({
+    loading: false,
+    repos: null,
+  });
+
+  useEffect(() => {
+    setAppState({ loading: true });
+    const apiUrl = `http://127.0.0.1:8000/api/product/?format=json`;
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((prod) => {
+        setAppState({ loading: false, prod: prod });
+      });
+  }, [setAppState]);
+  return (
+    <div className='App'>
+      <div className='container'>
+        <h1>My Repositories</h1>
+      </div>
+      <div className='repo-container'>
+        <ListLoading isLoading={appState.loading} prod={appState.prod} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
