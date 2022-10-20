3032ea6c837527f04a25ae16ac936a24fb178bba
DruceWin <kulakov1@tut.by>
2022-10-20 22:27:03 +0300

    change app.js v3

--- front_react_app/main/src/App.js
+++ front_react_app/main/src/App.js
@@ -1,123 +1,127 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
 
-const List = (props) => {
-  const { prod } = props;
-  if (!prod || prod.length === 0) return <p>No products, sorry</p>;
-  return (
-    <ul>
-      <h2 className='list-head'>Available Products</h2>
-      {prod.map((pr) => {
-        return (
-          <li key={pr.id} className='list'>
-            <span className='prod-text'>{pr.title}. </span>
-            <span className='prod-description'> Описание: {pr.description}.</span>
-            <span className='prod-price'> Цена: {pr.price} руб.</span>
-            {/*<span className='repo-category'> Категория: {pr.category_set}.</span>*/}
-          </li>
-        );
-      })}
-    </ul>
-  );
-};
-
-function withListLoading(Component) {
-  return function WihLoadingComponent({ isLoading, ...props }) {
-    if (!isLoading) return <Component {...props} />;
-    return (
-      <p style={{ textAlign: 'center', fontSize: '30px' }}>
-        Hold on, fetching data may take some time :)
-      </p>
-    );
-  };
-}
 
 function App() {
-  const ListLoading = withListLoading(List);
-  const [appState, setAppState] = useState({
-    loading: false,
-    repos: null,
-  });
-  const [product, setProduct] = useState(
-      [{title:'najmi po prodyctam'}])
-  const [category, setCategory] = useState(
-      [{name:'najmi po kategoriyam'}])
-  const [product_text, setProductText] = useState('a')
-  const [product_title, setProductTitle] = useState({"title":''})
-  function get_products(){
-    fetch('http://127.0.0.1:8000/api/product/')
-        .then((res) => res.json())
-        .then(products=>{
-            setProduct(products)
-            console.log(products)
-            console.log(product)
-        })
-  }
-  function get_categories(){
-    fetch('http://127.0.0.1:8000/api/categories/')
-        .then((res) => res.json())
-        .then(categories=>{
-            setCategory(categories)
-        })
-  }
+    const [product, setProduct] = useState(
+        [{title: 'najmi po prodyctam'}])
+    const [category, setCategory] = useState(
+        [{name: 'najmi po kategoriyam'}])
+    const [product_sort, setProductSort] = useState(
+        [{title: ''}])
 
-  function set_input_value_for_product_text(p){
-      setProductText(p.target.value)
-  }
+    useEffect(() => {
+        fetch('http://localhost:8000/api/categories/')
+            .then(response => response.json())
+            .then(data => {
+                setCategory(data)
+            })
+    }, [])
+    console.log(category)
+    useEffect(() => {
+        fetch('http://localhost:8000/api/product/')
+            .then(response => response.json())
+            .then(data => {
+                setProduct(data)
+            })
+    }, [])
 
-  function get_product_for_text(){
-      fetch(`http://127.0.0.1:8000/api/product/${product_text}`)
-        .then((res) => res.json())
-        .then(product=>{
-            if (!product.title){
-                setProductTitle({title: "net takogo producta"})
+    function sort_products(e) {
+        console.log(e.target.id)
+        let data_sorted = []
+        for(let i of product){
+            if(i.category.name===e.target.id){
+                data_sorted.push(i)
             }
-            else {setProductTitle(product)}
-        })
-  }
+        }
+        setProductSort(data_sorted)
+    }
 
-  useEffect(() => {
-    setAppState({ loading: true });
-    const apiUrl = `http://127.0.0.1:8000/api/product`;
-    fetch(apiUrl)
-      .then((res) => res.json())
-      .then((prod) => {
-        setAppState({ loading: false, prod: prod });
-      });
-  }, [setAppState]);
-  return (
-    <div className='App'>
-      <div className='container'>
-        <h1>My API</h1>
-      </div>
-        <input type="button" value={'получить продукты'} onClick={get_products}/>
-        <input type="button" value={'получить категории'} onClick={get_categories}/>
-      <div className="wrapper" style={{display:"flex"}}>
-          <div className="products" style={{margin:"15px"}}>
-              {product.map((item, i) => <p key={i}>{item.title}</p>)}
-          </div>
-          <div className="categories" style={{margin:"15px"}}>
-              {category.map((item, i) => <p key={i}>{item.name}</p>)}
-          </div>
-      </div>
-      <div>
-          <form action="">
-              <input type="text" placeholder="Введи название продукта" onChange={set_input_value_for_product_text}/>
-              <input type="button" value="OK" onClick={get_product_for_text}/>
-          </form>
-      </div>
-      <div style={{display:"flex", justifyContent:"center", alignContent:"center"}}>
-          <div>
-              <p>{product_title.title}</p>
-              <p>{product_title.description}</p>
-              <p>{product_title.price}</p>
-          </div>
-      </div>
-      <div className='repo-container'>
-        <ListLoading isLoading={appState.loading} prod={appState.prod} />
-      </div>
-    </div>
-  );
+    if (product_sort[0].title === '') {
+        return (
+            <div className='App'>
+                <h1>My API</h1>
+                <nav>
+                    <ul style={{
+                        display: 'flex',
+                        width: '80%',
+                        justifyContent: 'flex-start',
+                        background: 'blue'
+                    }}>
+                        {category.map(item => <li style={{margin: '20px', color: 'yellow', cursor: 'pointer'}}
+                                                  key={item.name}
+                                                  onClick={sort_products}
+                                                  id={item.name}>
+                            {item.name}
+                        </li>)}
+                    </ul>
+                </nav>
+                <h3>Продукты</h3>
+                <main style={{
+                    display: 'flex',
+                    width: '80%',
+                    justifyContent: 'flex-start',
+                    background: 'grey',
+                    padding: '20px'
+                }}>
+                    {product.map(item => <div style={{
+                        height: '150px',
+                        width: "150px",
+                        border: '1px solid red',
+                        background: 'goldenrod',
+                        margin: '20px'
+                    }}>
+                        <p>{item.title}</p><p>{item.price}</p>
+                    </div>)}
+                </main>
+            </div>
+        );
+    }
+    else {
+        return (
+            <div className='App'>
+                <h1>My API</h1>
+                <nav>
+                    <ul style={{
+                        display: 'flex',
+                        width: '80%',
+                        justifyContent: 'flex-start',
+                        background: 'blue'
+                    }}>
+                        {category.map(item => <li style={{
+                            margin: '20px',
+                            color: 'yellow',
+                            cursor: 'pointer'
+                        }}
+                                                  key={item.name}
+                                                  onClick={sort_products}
+                                                  id={item.name}>
+                            {item.name}
+                        </li>)}
+                        <li>All</li>
+                    </ul>
+                </nav>
+                <h3>Продукты</h3>
+                <main style={{
+                    display: 'flex',
+                    width: '80%',
+                    justifyContent: 'flex-start',
+                    background: 'grey',
+                    padding: '20px'
+                }}>
+                    {product_sort.map(item => <div style={{
+                        height: '150px',
+                        width: "150px",
+                        border: '1px solid red',
+                        background: 'goldenrod',
+                        margin: '20px'
+                    }}>
+                        <p>{item.title}</p>
+                        <p>{item.price}</p>
+                    </div>)}
+                </main>
+            </div>
+        );
+    }
 }
-
 export default App;
---
+++ front_react_app/main/src/Old_App.js
@@ -0,0 +1,123 @@
+import React, {useEffect, useState} from 'react';
+import './App.css';
+
+const List = (props) => {
+  const { prod } = props;
+  if (!prod || prod.length === 0) return <p>No products, sorry</p>;
+  return (
+    <ul>
+      <h2 className='list-head'>Available Products</h2>
+      {prod.map((pr) => {
+        return (
+          <li key={pr.id} className='list'>
+            <span className='prod-text'>{pr.title}. </span>
+            <span className='prod-description'> Описание: {pr.description}.</span>
+            <span className='prod-price'> Цена: {pr.price} руб.</span>
+            {/*<span className='repo-category'> Категория: {pr.category_set}.</span>*/}
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+function withListLoading(Component) {
+  return function WihLoadingComponent({ isLoading, ...props }) {
+    if (!isLoading) return <Component {...props} />;
+    return (
+      <p style={{ textAlign: 'center', fontSize: '30px' }}>
+        Hold on, fetching data may take some time :)
+      </p>
+    );
+  };
+}
+
+function App() {
+  const ListLoading = withListLoading(List);
+  const [appState, setAppState] = useState({
+    loading: false,
+    repos: null,
+  });
+  const [product, setProduct] = useState(
+      [{title:'najmi po prodyctam'}])
+  const [category, setCategory] = useState(
+      [{name:'najmi po kategoriyam'}])
+  const [product_text, setProductText] = useState('a')
+  const [product_title, setProductTitle] = useState({"title":''})
+  function get_products(){
+    fetch('http://127.0.0.1:8000/api/product/')
+        .then((res) => res.json())
+        .then(products=>{
+            setProduct(products)
+            console.log(products)
+            console.log(product)
+        })
+  }
+  function get_categories(){
+    fetch('http://127.0.0.1:8000/api/categories/')
+        .then((res) => res.json())
+        .then(categories=>{
+            setCategory(categories)
+        })
+  }
+
+  function set_input_value_for_product_text(p){
+      setProductText(p.target.value)
+  }
+
+  function get_product_for_text(){
+      fetch(`http://127.0.0.1:8000/api/product/${product_text}`)
+        .then((res) => res.json())
+        .then(product=>{
+            if (!product.title){
+                setProductTitle({title: "net takogo producta"})
+            }
+            else {setProductTitle(product)}
+        })
+  }
+
+  useEffect(() => {
+    setAppState({ loading: true });
+    const apiUrl = `http://127.0.0.1:8000/api/product`;
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((prod) => {
+        setAppState({ loading: false, prod: prod });
+      });
+  }, [setAppState]);
+  return (
+    <div className='App'>
+      <div className='container'>
+        <h1>My API</h1>
+      </div>
+        <input type="button" value={'получить продукты'} onClick={get_products}/>
+        <input type="button" value={'получить категории'} onClick={get_categories}/>
+      <div className="wrapper" style={{display:"flex"}}>
+          <div className="products" style={{margin:"15px"}}>
+              {product.map((item, i) => <p key={i}>{item.title}</p>)}
+          </div>
+          <div className="categories" style={{margin:"15px"}}>
+              {category.map((item, i) => <p key={i}>{item.name}</p>)}
+          </div>
+      </div>
+      <div>
+          <form action="front_react_app/main/src/Old_App">
+              <input type="text" placeholder="Введи название продукта" onChange={set_input_value_for_product_text}/>
+              <input type="button" value="OK" onClick={get_product_for_text}/>
+          </form>
+      </div>
+      <div style={{display:"flex", justifyContent:"center", alignContent:"center"}}>
+          <div>
+              <p>{product_title.title}</p>
+              <p>{product_title.description}</p>
+              <p>{product_title.price}</p>
+          </div>
+      </div>
+      <div className='repo-container'>
+        <ListLoading isLoading={appState.loading} prod={appState.prod} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
